939fee4b90d12977b9eee95ff2192f0c636d3c33
Kristin Johnson <kristin_johnson@harvard.edu>
2022-10-16 21:19:45 +0000

    week 7 examples

---
+++ practice/code-style.php
@@ -0,0 +1,8 @@
+<?php
+
+#camel case
+$shippingCosts = 0; 
+
+#snake case
+$shipping_costs = 0; 
+
---
+++ practice/high-low.php
@@ -0,0 +1,23 @@
+<?php
+
+function checkNumber($guess,$mysterNumber){
+
+    $example = 'abc';
+
+    if ($guess == $mysterNumber){
+        return 'correct';
+    }
+   elseif ($guess > $mysterNumber){
+        return 'high';
+    }
+    else {
+        return 'low';
+    }
+}
+
+//var_dump(checkNumber(3,6)); #low
+//var_dump(checkNumber(7,3)); #high
+//var_dump(checkNumber(3,3)); #correct 
+
+//var_dump($example); //will throw error because it is only scope of function
+
