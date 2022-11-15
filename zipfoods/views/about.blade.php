e32a34dba8aecb08b9310e5582d6d0462a0bcdce
Kristin Johnson <kristin_johnson@harvard.edu>
2022-11-15 22:45:24 +0000

    week11

--- zipfoods/views/about.blade.php
+++ zipfoods/views/about.blade.php
@@ -1,3 +1,14 @@
-<h1>About</h1>
+@extends('templates/master')
 
-{{$message}}
+@section ('title')
+About
+@endsection
+
+@section('head')
+<link href='/css/about.css' rel='stylesheet'>
+@endsection
+
+@section('content') <h2>About</h2>
+
+<p>Zipfoods is your one stop shop for convient online grocery shopping in the greater Boston area</p>
+@endsection
