0d73aaf24f70845b149b9d7698d2b5d19d95f7b3
Kristin Johnson <kristin_johnson@harvard.edu>
2022-11-15 23:49:19 +0000

    .

--- zipfoods/views/errors/missingProduct.blade.php
+++ zipfoods/views/errors/missingProduct.blade.php
@@ -5,8 +5,8 @@
 @endsection
 
 @section('content')
-<h2>Product Not Found</h2>
-<p>Sorry, we were not able to find the product you were looking for.</p>
-<p><a href = '/products'>Check out our other products..</a></p>
+    <h2>Product Not Found</h2>
+    <p>Sorry, we were not able to find the product you were looking for.</p>
+    <p><a href = '/products'>Check out our other products..</a></p>
 
 @endsection
