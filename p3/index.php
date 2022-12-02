ee5ce952e5a967974c998cfa80b1d13b62937ce1
Kristin Johnson <kristin_johnson@harvard.edu>
2022-12-02 02:38:28 +0000

    p3 file creation

--- p3/index.php
+++ p3/index.php
@@ -1,2 +1,18 @@
 <?php
+
 echo "project 3";
+
+# This is the main controller
+# All traffic should route through here
+
+# Define constants
+define('DOC_ROOT', $_SERVER['DOCUMENT_ROOT'].'/../');
+
+# Initialize Composer autoloading
+require DOC_ROOT.'/vendor/autoload.php';
+
+# Initialize App object
+$app = new E2\App();
+
+# Invoke router
+echo $app->route();
